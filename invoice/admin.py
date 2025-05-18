f55798d97341db5a7f6f5ab55d045a2a3ce438a4
valeriosegreto <valeriosegreto@gmail.com>
2025-05-19 01:22:33 +0200

    add class Vehicle

--- invoice/admin.py
+++ invoice/admin.py
@@ -7,6 +7,11 @@ class InvoiceAdmin(admin.ModelAdmin):
     list_display = ['company_name', 'invoice_number', 'invoice_date', 'freight_cost',
                     'vat_rate', 'vat_amount', 'total_amount', 'payment_due_date']
 
+class VehicleAdmin(admin.ModelAdmin):
+    list_display= ["company_own", "plate", "vehicle_category", "eur_category", "contract_type",
+                   "insurance_term_expires", "review_deadline", "bollo_deadline", "aci_card_deadline",
+                   "matched_driver"]
 
 # Register your models here.
 admin.site.register(models.Invoice, InvoiceAdmin)
+admin.site.register(models.Vehicle, VehicleAdmin)
