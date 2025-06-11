d320972b3eede8313cfbbc0925fe5fe24b78f6ea
Vallinox <35802597+Vallinox@users.noreply.github.com>
2025-06-11 11:51:21 +0000

    refactor vehicle add and edit templates for improved structure and AJAX handling

---
+++ vehicle/static/vehicle/javascript/add_vehicle.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("vehicle-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Evita il reload della pagina
+
+      let form = this;
+      let formData = new FormData(form);
+
+      fetch(form.action, {
+        method: "POST",
+        body: formData,
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            document
+              .getElementById("success-message")
+              .classList.remove("d-none");
+            form.reset(); // Pulisce il form dopo l'invio
+          } else {
+            alert("Errore nell'invio: " + JSON.stringify(data.errors));
+          }
+        })
+        .catch((error) => console.error("Errore:", error));
+    });
+});
---
+++ vehicle/static/vehicle/javascript/edit_vehicle.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("vehicle-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Evita il reload della pagina
+
+      let form = this;
+      let formData = new FormData(form);
+
+      fetch(form.action, {
+        method: "POST",
+        body: formData,
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            form.style.display = "none";
+            document.getElementById("card-edit").style.visibility = "hidden";
+
+            document
+              .getElementById("success-message")
+              .classList.remove("d-none");
+            form.reset(); // Pulisce il form dopo l'invio
+
+            let errorElements = document.querySelectorAll(".error-message");
+            errorElements.forEach((el) => el.remove());
+          } else {
+            alert("Errore nell'invio: " + JSON.stringify(data.errors));
+          }
+        })
+        .catch((error) => console.error("Errore:", error));
+    });
+});
