b0c3bc793c24cca430a819cedb128b27113352da
Vallinox <35802597+Vallinox@users.noreply.github.com>
2025-06-12 09:35:12 +0000

    refactor project settings to use MSW_SGR module name

--- MSW_SGR/manage.py
+++ MSW_SGR/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoice_SGR.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MSW_SGR.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- MSW_SGR/wsgi.py
+++ MSW_SGR/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoice_SGR.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MSW_SGR.settings')
 
 application = get_wsgi_application()
